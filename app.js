8e3cd9a49d79d59a2dc8af3949118ca550b09aad
stefanobras <stefanobras20@gmail.com>
2020-08-06 19:06:59 -0300

    Add cart view

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
 var productRouter = require('./routes/product');
+var cartRouter = require('./routes/cart');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
 app.use('/product', productRouter);
+app.use('/cart', cartRouter);
 
 
 // catch 404 and forward to error handler

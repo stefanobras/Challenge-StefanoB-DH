d16d4a2ddb7ec6e94cb7c50bba33d85e7ce17b06
stefanobras <stefanobras20@gmail.com>
2020-08-06 15:20:03 -0300

    Adds detailed product view

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var categoryRouter = require('./routes/category');
+var productRouter = require('./routes/product');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/category', categoryRouter);
+app.use('/product', productRouter);
 
 
 // catch 404 and forward to error handler
